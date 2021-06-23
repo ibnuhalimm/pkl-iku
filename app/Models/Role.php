d6d27d2b159100e07907bbd9e9aa9796c8ec22e5
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-06-23 07:50:48 +0700

    add softdeletes to roles table

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Role extends Model
 {
+    use SoftDeletes;
+
     /**
      * Disabled timestamps
      *
