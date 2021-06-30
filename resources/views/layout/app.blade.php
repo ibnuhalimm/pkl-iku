e2aaf033f9cb932621bf8e7a5f451c704b633c9a
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-06-30 11:16:45 +0700

    add some navigation menus

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -45,6 +45,29 @@
                                     </span>
                                 </x-sidebar.nav-link>
                             </x-sidebar.nav-item>
+                            <x-sidebar.nav-item :active="false" x-data="{ subnavOpen: false }">
+                                <x-sidebar.nav-link href="#" :active="false" x-on:click="subnavOpen = !subnavOpen">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key" viewBox="0 0 16 16">
+                                        <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8zm4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5z"/>
+                                        <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                                    </svg>
+                                    <span class="ml-3">
+                                        Program IKU
+                                    </span>
+                                    <span class="absolute left-auto right-3">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-3 h-3 bi bi-caret-down" viewBox="0 0 16 16">
+                                            <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
+                                        </svg>
+                                    </span>
+                                </x-sidebar.nav-link>
+                                <x-sidebar.subnav-wrapper x-show="subnavOpen">
+                                    <x-sidebar.subnav-item :active="false">
+                                        <x-sidebar.subnav-link href="#" :active="false">
+                                            Pekerjaan Layak
+                                        </x-sidebar.subnav-link>
+                                    </x-sidebar.subnav-item>
+                                </x-sidebar.subnav-wrapper>
+                            </x-sidebar.nav-item>
                             <x-sidebar.nav-item :active="false">
                                 <x-sidebar.nav-link href="#" :active="false">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-4 h-4 bi bi-people" viewBox="0 0 16 16">
@@ -65,6 +88,35 @@
                                         Biodata
                                     </span>
                                 </x-sidebar.nav-link>
+                            </x-sidebar.nav-item>
+
+                            <x-sidebar.nav-item :active="false" x-data="{ subnavOpen: false }">
+                                <x-sidebar.nav-link href="#" :active="false" x-on:click="subnavOpen = !subnavOpen">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-4 h-4 bi bi-building" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"/>
+                                        <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"/>
+                                    </svg>
+                                    <span class="ml-3">
+                                        Perusahaan
+                                    </span>
+                                    <span class="absolute left-auto right-3">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-3 h-3 bi bi-caret-down" viewBox="0 0 16 16">
+                                            <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
+                                        </svg>
+                                    </span>
+                                </x-sidebar.nav-link>
+                                <x-sidebar.subnav-wrapper x-show="subnavOpen">
+                                    <x-sidebar.subnav-item :active="false">
+                                        <x-sidebar.subnav-link href="#" :active="false">
+                                            Data Perusahaan
+                                        </x-sidebar.subnav-link>
+                                    </x-sidebar.subnav-item>
+                                    <x-sidebar.subnav-item :active="false">
+                                        <x-sidebar.subnav-link href="#" :active="false">
+                                            Kategori
+                                        </x-sidebar.subnav-link>
+                                    </x-sidebar.subnav-item>
+                                </x-sidebar.subnav-wrapper>
                             </x-sidebar.nav-item>
                             <x-sidebar.nav-item :active="false" x-data="{ subnavOpen: false }">
                                 <x-sidebar.nav-link href="#" :active="false" x-on:click="subnavOpen = !subnavOpen">
@@ -97,6 +149,17 @@
                                         </x-sidebar.subnav-link>
                                     </x-sidebar.subnav-item>
                                 </x-sidebar.subnav-wrapper>
+                            </x-sidebar.nav-item>
+                            <x-sidebar.nav-item>
+                                <x-sidebar.nav-link>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-4 h-4 bi bi-person-check" viewBox="0 0 16 16">
+                                        <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                                        <path fill-rule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                                    </svg>
+                                    <span class="ml-3">
+                                        User & Hak Akses
+                                    </span>
+                                </x-sidebar.nav-link>
                             </x-sidebar.nav-item>
                             <x-sidebar.nav-item>
                                 <x-sidebar.nav-link :href="route('logout')" :active="false">
