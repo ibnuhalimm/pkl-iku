3647f54ae9f84a0334edca37165f3ffb3b11cf38
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-06-30 12:19:21 +0700

    increase padding-left of subnav

--- resources/views/components/sidebar/subnav-link.blade.php
+++ resources/views/components/sidebar/subnav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
     $cssClass = $active ?? false
-            ? 'block w-full pl-11 pr-2 py-2 rounded-md hover:bg-blue-400 hover:bg-opacity-20 text-blue-600'
-            : 'block w-full pl-11 pr-2 py-2 rounded-md hover:bg-blue-400 hover:bg-opacity-20 text-gray-600'
+            ? 'block w-full pl-12 pr-2 py-2 rounded-md hover:bg-blue-400 hover:bg-opacity-20 text-blue-600'
+            : 'block w-full pl-12 pr-2 py-2 rounded-md hover:bg-blue-400 hover:bg-opacity-20 text-gray-600'
 @endphp
 
 <a {{ $attributes->merge([
